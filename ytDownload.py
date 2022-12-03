4284e946f3327c6d2ca381b81e1639162e149acb
Ranset <ransetfleites0@gmail.com>
2022-12-02 19:46:10 -0500

    mejoras en el codigo

--- ytDownload.py
+++ ytDownload.py
@@ -26,12 +26,29 @@ class DownTube:
 
             for link in links:
                 nro += 1
-                print(f'fetching {nro} de {total}')
-                downUrl += link.url
+                print(f'fetching {nro} de {total} - {link.title} dur {link.vid_info}')
+                downUrl += link.url + ' '
+
+        print(downUrl)
+
+    def get_list(self):
+        p = Playlist('https://www.youtube.com/watch?v=5FsBXcrQ1XI&list=PLyDw0WMdjWprMilid88cuq85JX_oIW9lK')
+        print(f'Fetching lista: {p.title}')
+        total = p.length
+        nro = 0
+        downUrl = ''
+
+        for url in p.video_urls:
+            yt = YouTube(url)
+            print(f'fetching {nro} de {total} - {yt.title} dur: {yt.length}')
+            links = yt.streams.filter(progressive=True, res='720p')
+
+            for link in links:
+                downUrl += link.url + ' '
 
         print(downUrl)
 
 if __name__ == "__main__":
     tube = DownTube()
     #tube.get_video_url()
-    tube.get_list_titles()
+    tube.get_list()
